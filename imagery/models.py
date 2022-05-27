826afd9edf82e0ed54d431db4bc42d2cc5b1031e
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-05-27 22:28:22 +0300

    feature:add the templates and urls

--- imagery/models.py
+++ imagery/models.py
@@ -1,3 +1,26 @@
+
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+    name=models.CharField(max_length=100)
+    image_post=models.ImageField(upload_to='image/',null=True)
+    description=models.TextField()
+    location=models.ForeignKey('Location',on_delete=models.CASCADE,)
+    category=models.ForeignKey('Category',on_delete=models.CASCADE)
+
+    def __str__(self) :
+        return self.name
+
+class Location(models.Model):
+    distance=models.CharField(max_length=50)
+
+    def __str__(self) :
+        return self.distance
+
+
+class Category(models.Model):
+    title=models.CharField(max_length=100)
+    content=models.TextField(max_length=300)
+    
+    def __str__(self) :
+        return self.name
