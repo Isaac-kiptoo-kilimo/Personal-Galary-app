1631022daa79c511976deefeb9cd633be0d69b7c
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-05-28 09:57:02 +0300

    feature:added single image,add image files

--- imagery/urls.py
+++ imagery/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('images/',views.get_images,name='images'),
+    path('image/',views.get_single_image,name='image'),
+    path('addimage/',views.addimage,name='addimage'),
 ]
--- imagery/views.py
+++ imagery/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     return render (request,'pages/index.html')
+
+def get_images(request):
+    return render (request,'pages/images.html')
+
+def get_single_image(request):
+    return render (request,'pages/single.html')
+
+def addimage(request):
+    return render(request,'pages/addimage.html')
